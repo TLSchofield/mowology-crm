988d6236defd7371e717ad614849fa68b038a867
Tim Schofield <timschofield@example.com>
2026-03-06 15:34:45 -0800

    Bump mowology-brand.css cache buster to 20260306e (layout manager styles)

--- public/crm/includes/appstack_head.php
+++ public/crm/includes/appstack_head.php
@@ -83,7 +83,7 @@ $bodyClass  = $bodyClass  ?? '';
   <link href="/crm/css/classic.css" rel="stylesheet">
 
   <!-- Mowology brand override -->
-  <link href="/crm/css/mowology-brand.css?v=20260306d" rel="stylesheet">
+  <link href="/crm/css/mowology-brand.css?v=20260306e" rel="stylesheet">
 
   <!-- Global mobile navigation bars (top bar + bottom bar + slide-up menu) -->
   <link href="/crm/css/mobile-nav.css?v=20260303d" rel="stylesheet">
